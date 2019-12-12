a9469c592b04f0ceb2fd1913c2f71ea77ee2909e
Dar Sissau <dar.sissau@student.howest.be>
2019-12-12 22:12:55 +0100

    cached fetches better with serviceworker 2

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,30 +12,32 @@ self.addEventListener('install', (event) => {
 
 // On network response
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        // Try the network
-        fetch(event.request)
-            .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                    .then(function (cache) {
-                        // Put in cache if succeeds
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                    })
-            })
-            .catch(function (err) {
-                // Fallback to cache
-                return caches.match(event.request);
-            })
-    );
-    // event.respondWith(async function () {
-    //     const cache = await caches.open('todo-app');
-    //     const cachedResponse = await cache.match(event.request);
-    //     if (cachedResponse) return cachedResponse;
-    //     const networkResponse = await fetch(event.request);
-    //     event.waitUntil(
-    //         cache.put(event.request, networkResponse.clone())
-    //     );
-    //     return networkResponse;
-    // }());
+    // event.respondWith(
+    //     // Try the network
+    //     fetch(event.request)
+    //         .then(function (res) {
+    //             return caches.open('todo-app')
+    //                 .then(function (cache) {
+    //                     // Put in cache if succeeds
+    //                     cache.put(event.request.url, res.clone());
+    //                     return res;
+    //                 })
+    //         })
+    //         .catch(function (err) {
+    //             // Fallback to cache
+    //             console.log("IK NEEM DE CACHE")
+    //             return caches.match(event.request);
+    //         })
+    // );
+
+    event.respondWith(async function () {
+        const cache = await caches.open('todo-app');
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) return cachedResponse;
+        const networkResponse = await fetch(event.request);
+        event.waitUntil(
+            cache.put(event.request, networkResponse.clone())
+        );
+        return networkResponse;
+    }());
 });
